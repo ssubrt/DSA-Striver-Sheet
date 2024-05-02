f88b2b79136b9c7706da1887fa88bf70c2b79ad0
ssubrt <subratgangwar03@gmail.com>
2024-05-02 17:36:44 +0530

    BinarySearch

---
+++ Day-6/binarySearch.cpp
@@ -0,0 +1,37 @@
+------------>>>>>>>>>> Approach 1  <<<<<<<<<<<<<<-----------
+// T.C - O(logN)
+
+int binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+
+--------------->>>>>>>>>>>>>>> Approach 2 <<<<<<<<<<<<<<<<<<<<<---------------
+
+// T.C - O(logN)
+
+int binarySearch(vector<int>& nums, int low, int high, int target) {
+
+    if (low > high) return -1; //Base case.
+
+    // Perform the steps:
+    int mid = (low + high) / 2;
+    if (nums[mid] == target) return mid;
+    else if (target > nums[mid])
+        return binarySearch(nums, mid + 1, high, target);
+    return binarySearch(nums, low, mid - 1, target);
+}
+
+int search(vector<int>& nums, int target) {
+    return binarySearch(nums, 0, nums.size() - 1, target);
+}
