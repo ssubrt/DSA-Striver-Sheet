c2439b0fa8c7de6c1c3d6214712c1b13aaaad1fb
ssubrt <subratgangwar03@gmail.com>
2024-05-03 16:51:33 +0530

    Recursion

---
+++ Day-7/LowerBound.cpp
@@ -0,0 +1,38 @@
+----------------->>>>>>>>>>>>>>> Approach 1 Brute Force  <<<<<<<<<<<<<----------------
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+int lowerBound(vector<int> arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            // lower bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+
+
+--------------------->>>>>>>>>>>>>>> Approach 2 Optimial <<<<<<<<<<<<<<<<<<<-----------------
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
