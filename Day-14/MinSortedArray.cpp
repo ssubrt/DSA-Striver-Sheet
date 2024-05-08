7de927efe1f0315500523d85103c398c812cd0a7
ssubrt <subratgangwar03@gmail.com>
2024-05-08 12:00:11 +0530

    binarySearch

---
+++ Day-14/MinSortedArray.cpp
@@ -0,0 +1,93 @@
+------------>>>>>>>>>>>>>>>>> Approach -1 <<<<<<<<<<<<<<<------------------
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+int findMin(vector<int>& arr) {
+    int n = arr.size(); // size of the array.
+    int mini = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        // Always keep the minimum.
+        mini = min(mini, arr[i]);
+    }
+    return mini;
+}
+
+------------>>>>>>>>>> Approach -2 <<<<<<<<<<<<<<<<<<<-----------------
+
+// Time Complexity: O(logN)
+// Space Complexity: O(1)   
+
+int findMin(vector<int>& arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            ans = min(ans, arr[low]);
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            ans = min(ans, arr[mid]);
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+
+------------>>>>>>>>>> Approach -3 <<<<<<<<<<<<<<<<<<<-----------------
+
+// Time Complexity: O(logN)
+// Space Complexity: O(1)   
+
+int findMin(vector<int>& arr) {
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
+            ans = min(ans, arr[low]);
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            ans = min(ans, arr[low]);
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else { //if right part is sorted:
+
+            // keep the minimum:
+            ans = min(ans, arr[mid]);
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+---------->>>>>>>>>>>>>>>>>>>> Approach -4 <<<<<<<<<<<<<<<<<<<<<<<----------------
+// Time Complexity: O(NlogN)
+// Space Complexity: O(1)
+
+int findMin(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[0];
+ }
+
