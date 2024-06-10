bf29e29fc770d1c78307c2c19fe64d0b4bed9772
Subrat <subratgangwar03@gmail.com>
2024-06-10 12:05:43 +0530

    Day-40

---
+++ Day-40/Longest Palindromic Substring.cpp
@@ -0,0 +1,34 @@
+--------------------->>>>>>>>>>>>>>>> Approach <<<<<<<<<<<<<<<<<<<<<<<<<----------------------
+// time complexity of this code is O(n^3)
+// space complexity of this code is O(n)
+
+
+class Solution {
+public:
+ bool isPalindrome(string &s,int start,int end){
+     while(start<end){
+         if(s[start]!= s[end]){
+             return false;
+         }
+         start++;
+         end--;
+     }
+     return true;
+ }
+
+    string longestPalindrome(string s) {
+        string ans ="";
+
+        for(int i=0;i<s.size();i++){
+            for(int j =i;j<s.size();j++){
+                if(isPalindrome(s,i,j)){
+                    string t = s.substr(i,j-i+1);
+                    ans = t.size()>ans.size()?t:ans;
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+};
